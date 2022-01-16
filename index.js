84da803022056bb7554acff96ab41c53ebaab4d3
hcwtam <hcwtam@gmail.com>
2022-01-16 12:32:46 +0100

    Translate word on highlight using google translate api, setup webpack

--- index.js
+++
@@ -1,9 +0,0 @@
-function logSelection(event) {
-    const selection = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd);
-  }
-  
-  document.addEventListener('mouseup', (event) => {
-    console.log('Selected text:', window.getSelection().toString());
-    console.log(window.getSelection().getRangeAt(0).getBoundingClientRect());
-  });
-  
---
+++ src/index.js
@@ -0,0 +1,54 @@
+import axios from "axios";
+
+const styles = {
+  backgroundColor: "#111",
+  color: "#fff",
+  padding: "10px",
+  position: "absolute",
+};
+
+function setStyle(element) {
+  for (let key in styles) {
+    element.style[key] = styles[key];
+  }
+};
+
+async function getTranslation(text, targetLanguage) {
+  const res = await axios.post("https://translation.googleapis.com/language/translate/v2?key=KEY", {
+    q: text,
+    target: targetLanguage,
+  }).catch(e => {console.log(e);});
+  return res.data.data.translations[0].translatedText; // res.data.data.translations[0] = { translatedText: "Hallo", detectedSourceLanguage: "en" }
+}
+
+function removeTranslateElement() {
+  const translateElements = document.getElementsByClassName("translation-element");
+  if (translateElements.length > 0) translateElements[0].remove();
+}
+
+async function doubleClickToTranslate(event) {
+  removeTranslateElement();
+
+  const selectedString = window.getSelection().toString();
+  if (selectedString) {
+    const selectedElement = event.target;
+    const parent = selectedElement.parentNode;
+
+    const selectionRect = window
+      .getSelection()
+      .getRangeAt(0)
+      .getBoundingClientRect(); // get selected string position (DOMRect object)
+
+    // create translation element
+    const translationElement = document.createElement("span");
+    translationElement.className = "translation-element";
+    setStyle(translationElement);
+    translationElement.style.left = selectionRect.left.toString() + "px";
+    translationElement.style.top = (selectionRect.top - 45).toString() + "px";
+    const translatedText = await getTranslation(selectedString, "zh-TW");
+    translationElement.textContent = translatedText;
+    parent.insertBefore(translationElement, selectedElement);
+  }
+};
+
+document.addEventListener("mouseup", doubleClickToTranslate);
